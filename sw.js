fbc8d8d1b2c476630ceff0feb776c89ae3c0c909
Anna-2008 <anna.c.santos40@aluno.senai.br>
2026-05-05 14:12:17 -0300

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,36 +1,32 @@
-const CACHE_NAME = 'gelato-front';
+const CACHE_NAME = 'gelato-cache-v3';
 const ASSETS = [
     './',
     './index.html',
-    './manifest.json',
-    './images/logo.webp',
-    './images/logo64.png',
-    './images/logo512.png',
-    './images/banner.webp',
-    './images/WPP.webp'
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('Fazendo whey no cache!');
+        caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(ASSETS);
         })
     );
 });
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
-self.addEventListener('activate', (event) => {
+
+self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then((keys) => {
+        caches.keys().then(keys => {
             return Promise.all(
                 keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
             );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
